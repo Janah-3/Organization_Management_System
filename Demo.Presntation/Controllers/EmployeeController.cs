1ecff8ada5b97988d3723f2e2d3f4abbff84a072
jana <jana.ayoub.004@gmail.com>
2025-04-04 01:41:12 +0200

    part 7 edit action

--- Demo.Presntation/Controllers/EmployeeController.cs
+++ Demo.Presntation/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using Demo.BusinessLogic.DataTransferObjects.EmployeeDto;
 using Demo.BusinessLogic.Services;
+using Demo.DataAccess.Models.Shared.Enum;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Presntation.Controllers
@@ -53,6 +54,64 @@ namespace Demo.Presntation.Controllers
                 var employee = _employeeService.GetEmployeeById(id.Value);
                 return employee is null ? NotFound() : View(employee);
             
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(!id.HasValue) return BadRequest();
+
+            var employee = _employeeService.GetEmployeeById(id.Value);
+            if (employee is null) return NotFound();
+
+            var employeeDto = new UpdatedEmployeeDto()
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                salary = employee.salary,
+                Address = employee.Address,
+                Age = employee.Age,
+                PhoneNumber = employee.PhoneNumber,
+                IsActive = employee.IsActive,
+                HiringDate = employee.HiringDate,
+
+            };
+
+            return View(employeeDto);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int? id ,UpdatedEmployeeDto employeeDto)
+        {
+
+            if (!id.HasValue || id!= employeeDto.Id) return BadRequest();
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int result = _employeeService.UpdateEmployee(employeeDto);
+                    if (result > 0)
+                        return RedirectToAction("Index");
+                    else
+                        ModelState.AddModelError("", "Failed to update employee.");
+                }
+                catch (Exception ex)
+                {
+                    if (Environment.IsDevelopment())
+                    {
+                        ModelState.AddModelError("", ex.Message);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "An error occurred while updating the employee.");
+                        Logger.LogError(ex, "An error occurred while updating the employee.");
+                    }
+                }
+            }
+            return View(employeeDto);
         }
 
 
