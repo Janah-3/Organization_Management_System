f034b17d755f4ca54f6660341ecb609c63e89d7c
jana <jana.ayoub.004@gmail.com>
2025-04-12 02:17:41 +0200

    Part 06 Install Package and DbContext

--- Demo.DataAccess/Data/contexts/AppDbContext.cs
+++ Demo.DataAccess/Data/contexts/AppDbContext.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Demo.DataAccess.Models.DepartmentModel;
+using Demo.DataAccess.Models.IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Demo.DataAccess.Data.contexts
 {
 
-    public class AppDbContext: DbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        { 
-        }
+       public  DbSet<ApplicationUser> users { get; set; }
+        public DbSet<IdentityRole> roles { get; set; }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
@@ -26,7 +24,9 @@ namespace Demo.DataAccess.Data.contexts
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());//i can use this line of code to apply all configurations in the assembly when the dbcontext class and the dataAccess are in the same project 
-           // modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);//i can use this line of code to apply all configurations in the assembly when the dbcontext class and the dataAccess are in the different projects
+                                                                                          // modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);//i can use this line of code to apply all configurations in the assembly when the dbcontext class and the dataAccess are in the different projects
+
+            base.OnModelCreating(modelBuilder);
         }
 
 
